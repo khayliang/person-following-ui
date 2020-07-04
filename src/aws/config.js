986accf7bc0685b992b9e490065444cb16a55905
khayliang <khayliang@gmail.com>
2020-07-04 16:57:37 +0800

    add aws

---
+++ src/aws/config.js
@@ -0,0 +1,11 @@
+const config = {
+    // DescribeSignalingChannel API can also be used to get the ARN from a channel name.
+    channelARN = 'CHANNEL_ARN_GOES_HERE',
+
+    // AWS Credentials
+    accessKeyId = 'ACCESS_KEY_ID_GOES_HERE',
+    secretAccessKey = 'SECRET_ACCESS_KEY_GOES_HERE',
+
+    region = 'us-west-2',
+    clientId = 'RANDOM_VALUE'
+}
